f1f82a21a46cf5019dd4c1af6542d9f92ad5c2c3
soroz30 <soroz30@yahoo.pl>
2017-09-25 12:08:31 +0200

    correction

--- workspace/js/scripts.js
+++ workspace/js/scripts.js
@@ -5,9 +5,9 @@ var value = (a * a) + (2 * a * b) - (b * b);
 console.log(value);
 
 if (value > 0) {
-	console.log("wynik dodatni");
+    console.log("wynik dodatni");
 } else if (value < 0) {
-	console.log("wynik ujemny");
+    console.log("wynik ujemny");
 } else {
-	console.log("wynik równy zero");
+    console.log("wynik równy zero");
 }
