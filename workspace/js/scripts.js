e9997be3c0aab0e7e5edb718097cf4a1694494d9
soroz30 <soroz30@yahoo.pl>
2017-09-25 16:48:06 +0200

    strings correction

--- workspace/js/scripts.js
+++ workspace/js/scripts.js
@@ -1,13 +1,13 @@
-var a = Number(prompt("Enter number a"));
-var b = Number(prompt("Enter number b"));
+var a = Number(prompt('Enter number a'));
+var b = Number(prompt('Enter number b'));
 var value = (a * a) + (2 * a * b) - (b * b);
 
 console.log(value);
 
 if (value > 0) {
-    console.log("wynik dodatni");
+    console.log('wynik dodatni');
 } else if (value < 0) {
-    console.log("wynik ujemny");
+    console.log('wynik ujemny');
 } else {
-    console.log("wynik równy zero");
+    console.log('wynik równy zero');
 }
