3718466c02e5fd163a95205dd2ef0992a2096acd
soroz30 <soroz30@yahoo.pl>
2017-09-25 12:02:14 +0200

    Zadanie 8.6

---
+++ workspace/js/scripts.js
@@ -0,0 +1,13 @@
+var a = Number(prompt("Enter number a"));
+var b = Number(prompt("Enter number b"));
+var value = (a * a) + (2 * a * b) - (b * b);
+
+console.log(value);
+
+if (value > 0) {
+	console.log("wynik dodatni");
+} else if (value < 0) {
+	console.log("wynik ujemny");
+} else {
+	console.log("wynik równy zero");
+}
